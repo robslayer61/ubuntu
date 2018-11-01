32f65e58f747128cda2335bad54b38077658e904
robslayer61 <44686090+robslayer61@users.noreply.github.com>
2018-11-01 20:09:23 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu Primeiro Script
+
+#Listando os diretórios do Linux
+ls -lha /
+read 
+clear
+
+#Atualizando o Apt ou Apt-Get
+apt update
+echo "listas atualizadas!!!"
+read
+cls
+
+#Desligando o servidor
+shutdown -h now
